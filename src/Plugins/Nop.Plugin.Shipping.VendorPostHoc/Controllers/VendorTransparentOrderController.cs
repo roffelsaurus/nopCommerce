07dc5b3bd051d1406e3b906bf947330e2e1ab2e2
Asbjørn Lucassen <roffelsaurus@gmail.com>
2018-08-14 22:38:27 +0200

    vendor transparency in order panels

---
+++ src/Plugins/Nop.Plugin.Shipping.VendorPostHoc/Controllers/VendorTransparentOrderController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Orders;
+using Nop.Services.Catalog;
+using Nop.Services.Common;
+using Nop.Services.ExportImport;
+using Nop.Services.Helpers;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Media;
+using Nop.Services.Messages;
+using Nop.Services.Orders;
+using Nop.Services.Payments;
+using Nop.Services.Security;
+using Nop.Services.Shipping;
+using Nop.Web.Areas.Admin.Controllers;
+using Nop.Web.Areas.Admin.Factories;
+using Nop.Web.Areas.Admin.Models.Orders;
+using Nop.Web.Framework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Plugin.Shipping.VendorPostHoc.Controllers
+{
+    public class VendorTransparentOrderController : OrderController
+    {
+        private readonly IOrderService _orderService;
+        private readonly IPermissionService _permissionService;
+        private readonly IOrderModelFactory _orderModelFactory;
+        private readonly IWorkContext _workContext;
+
+        private readonly decimal _allowedTotalChange;
+
+
+        public VendorTransparentOrderController(IAddressAttributeParser addressAttributeParser, IAddressService addressService, ICustomerActivityService customerActivityService, IDateTimeHelper dateTimeHelper, IDownloadService downloadService, IEncryptionService encryptionService, IExportManager exportManager, IGiftCardService giftCardService, ILocalizationService localizationService, IOrderModelFactory orderModelFactory, IOrderProcessingService orderProcessingService, IOrderService orderService, IPaymentService paymentService, IPdfService pdfService, IPermissionService permissionService, IPriceCalculationService priceCalculationService, IProductAttributeFormatter productAttributeFormatter, IProductAttributeParser productAttributeParser, IProductAttributeService productAttributeService, IProductService productService, IShipmentService shipmentService, IShippingService shippingService, IShoppingCartService shoppingCartService, IWorkContext workContext, IWorkflowMessageService workflowMessageService, OrderSettings orderSettings) : base(addressAttributeParser, addressService, customerActivityService, dateTimeHelper, downloadService, encryptionService, exportManager, giftCardService, localizationService, orderModelFactory, orderProcessingService, orderService, paymentService, pdfService, permissionService, priceCalculationService, productAttributeFormatter, productAttributeParser, productAttributeService, productService, shipmentService, shippingService, shoppingCartService, workContext, workflowMessageService, orderSettings)
+        {
+            _orderService = orderService;
+            _permissionService = permissionService;
+            _orderModelFactory = orderModelFactory;
+            _workContext = workContext;
+
+            // hardcoded for now based on paypals 115% change allowed
+            _allowedTotalChange = 1.15m;
+        }
+
+        /// <summary>
+        /// Overrides default controller action, allowing vendors to save order totals(with caveats).
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost, ActionName("Edit")]
+        [FormValueRequired("btnSaveOrderTotals")]
+        public override IActionResult EditOrderTotals(int id, OrderModel model)
+        {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManageOrders))
+                return AccessDeniedView();
+
+            //try to get an order with the specified id
+            var order = _orderService.GetOrderById(id);
+            if (order == null)
+                return RedirectToAction("List");
+            
+            // vendors can only edit shipping and it is subject to validation.
+            if (_workContext.CurrentVendor != null
+                && OrderShippingChangeValidation(order.OrderTotal,order.OrderShippingExclTax,model.OrderShippingExclTaxValue))
+            {
+                order.OrderShippingExclTax = model.OrderShippingExclTaxValue;
+            }
+            else
+            {
+                order.OrderSubtotalInclTax = model.OrderSubtotalInclTaxValue;
+                order.OrderSubtotalExclTax = model.OrderSubtotalExclTaxValue;
+                order.OrderSubTotalDiscountInclTax = model.OrderSubTotalDiscountInclTaxValue;
+                order.OrderSubTotalDiscountExclTax = model.OrderSubTotalDiscountExclTaxValue;
+                order.OrderShippingInclTax = model.OrderShippingInclTaxValue;
+                order.OrderShippingExclTax = model.OrderShippingExclTaxValue;
+                order.PaymentMethodAdditionalFeeInclTax = model.PaymentMethodAdditionalFeeInclTaxValue;
+                order.PaymentMethodAdditionalFeeExclTax = model.PaymentMethodAdditionalFeeExclTaxValue;
+                order.TaxRates = model.TaxRatesValue;
+                order.OrderTax = model.TaxValue;
+                order.OrderDiscount = model.OrderTotalDiscountValue;
+                order.OrderTotal = model.OrderTotalValue;
+            }
+            
+            _orderService.UpdateOrder(order);
+
+            //add a note
+            order.OrderNotes.Add(new OrderNote
+            {
+                Note = "Order totals have been edited",
+                DisplayToCustomer = false,
+                CreatedOnUtc = DateTime.UtcNow
+            });
+            _orderService.UpdateOrder(order);
+            LogEditOrder(order.Id);
+
+            //prepare model
+            model = _orderModelFactory.PrepareOrderModel(model, order);
+
+            return View(model);
+        }
+
+        private bool OrderShippingChangeValidation(decimal orderTotal, decimal orderShippingExclTax, decimal orderShippingExclTaxValue)
+        {
+            var newtotalWithShipping = orderTotal - orderShippingExclTax + orderShippingExclTaxValue;
+
+            var changeOfOldTotal = newtotalWithShipping / orderTotal;
+
+            return orderShippingExclTaxValue >= 0m
+                && changeOfOldTotal <= _allowedTotalChange;
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Shipping.VendorPostHoc/RouteProvider.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Nop.Web.Framework.Mvc.Routing;
+
+namespace Nop.Plugin.Shipping.VendorPostHoc
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        /// <summary>
+        /// Register routes
+        /// </summary>
+        /// <param name="routeBuilder">Route builder</param>
+        public void RegisterRoutes(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.PDTHandler", "Admin/PaymentPayPalStandard/PDTHandler",
+                 new { controller = "PaymentPayPalStandard", action = "PDTHandler" });
+
+//namespace Nop.Plugin.Misc.MyPlugin.Filters
+//    {
+//        public class MyFilterProvider : IFilterProvider
+//        {
+//            private readonly IActionFilter _actionFilter;
+
+//            public MyFilterProvider(IActionFilter actionFilter)
+//            {
+//                _actionFilter = actionFilter;
+//            }
+
+//            public IEnumerable<Filter> GetFilters(ControllerContext controllerContext,
+//                ActionDescriptor actionDescriptor)
+//            {
+//                if (actionDescriptor.ControllerDescriptor.ControllerType == typeof(OrderController) &&
+//                    actionDescriptor.ActionName.Equals("AddProductToOrderDetails") &&
+//                    controllerContext.HttpContext.Request.HttpMethod == "POST")
+//                {
+//                    return new Filter[]
+//                    {
+//                    new Filter(_actionFilter, FilterScope.Action, null)
+//                    };
+//                }
+
+//                return new Filter[] { };
+//            }
+//        }
+//    }
+
+
+    ////PDT
+    //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.PDTHandler", "Plugins/PaymentPayPalStandard/PDTHandler",
+    //     new { controller = "PaymentPayPalStandard", action = "PDTHandler" });
+
+    ////IPN
+    //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.IPNHandler", "Plugins/PaymentPayPalStandard/IPNHandler",
+    //     new { controller = "PaymentPayPalStandard", action = "IPNHandler" });
+
+    ////Cancel
+    //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.CancelOrder", "Plugins/PaymentPayPalStandard/CancelOrder",
+    //     new { controller = "PaymentPayPalStandard", action = "CancelOrder" });
+}
+
+        /// <summary>
+        /// Gets a priority of route provider
+        /// </summary>
+        public int Priority
+        {
+            get { return -1; }
+        }
+    }
+}
