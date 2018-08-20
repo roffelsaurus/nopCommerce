effa6a7dce960ba0c64d6b7d296a92ec73e91352
Asbjørn Lucassen <roffelsaurus@gmail.com>
2018-08-20 20:03:46 +0200

    wip vendor after big merge

--- src/Plugins/Nop.Plugin.Shipping.VendorPostHoc/RouteProvider.cs
+++ src/Plugins/Nop.Plugin.Shipping.VendorPostHoc/RouteProvider.cs
@@ -1,71 +1,71 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
-using Nop.Web.Framework.Mvc.Routing;
+﻿//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Routing;
+//using Nop.Web.Framework.Mvc.Routing;
 
-namespace Nop.Plugin.Shipping.VendorPostHoc
-{
-    public partial class RouteProvider : IRouteProvider
-    {
-        /// <summary>
-        /// Register routes
-        /// </summary>
-        /// <param name="routeBuilder">Route builder</param>
-        public void RegisterRoutes(IRouteBuilder routeBuilder)
-        {
-            routeBuilder.MapRoute("VendorEditOrderTotals", "Admin/Order/Edit/{id}",
-    new { controller = "VendorTransparentOrder", action = "EditOrderTotals" });
-            //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.PDTHandler", "Admin/PaymentPayPalStandard/PDTHandler",
-            //     new { controller = "PaymentPayPalStandard", action = "PDTHandler" });
-
-//namespace Nop.Plugin.Misc.MyPlugin.Filters
+//namespace Nop.Plugin.Shipping.VendorPostHoc
+//{
+//    public partial class RouteProvider : IRouteProvider
 //    {
-//        public class MyFilterProvider : IFilterProvider
+//        /// <summary>
+//        /// Register routes
+//        /// </summary>
+//        /// <param name="routeBuilder">Route builder</param>
+//        public void RegisterRoutes(IRouteBuilder routeBuilder)
 //        {
-//            private readonly IActionFilter _actionFilter;
+//    //        routeBuilder.MapRoute("VendorEditOrderTotals", "Admin/Order/Edit/{id}",
+//    //new { controller = "VendorTransparentOrder", action = "EditOrderTotals" });
+//            //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.PDTHandler", "Admin/PaymentPayPalStandard/PDTHandler",
+//            //     new { controller = "PaymentPayPalStandard", action = "PDTHandler" });
 
-//            public MyFilterProvider(IActionFilter actionFilter)
-//            {
-//                _actionFilter = actionFilter;
-//            }
+////namespace Nop.Plugin.Misc.MyPlugin.Filters
+////    {
+////        public class MyFilterProvider : IFilterProvider
+////        {
+////            private readonly IActionFilter _actionFilter;
 
-//            public IEnumerable<Filter> GetFilters(ControllerContext controllerContext,
-//                ActionDescriptor actionDescriptor)
-//            {
-//                if (actionDescriptor.ControllerDescriptor.ControllerType == typeof(OrderController) &&
-//                    actionDescriptor.ActionName.Equals("AddProductToOrderDetails") &&
-//                    controllerContext.HttpContext.Request.HttpMethod == "POST")
-//                {
-//                    return new Filter[]
-//                    {
-//                    new Filter(_actionFilter, FilterScope.Action, null)
-//                    };
-//                }
+////            public MyFilterProvider(IActionFilter actionFilter)
+////            {
+////                _actionFilter = actionFilter;
+////            }
 
-//                return new Filter[] { };
-//            }
+////            public IEnumerable<Filter> GetFilters(ControllerContext controllerContext,
+////                ActionDescriptor actionDescriptor)
+////            {
+////                if (actionDescriptor.ControllerDescriptor.ControllerType == typeof(OrderController) &&
+////                    actionDescriptor.ActionName.Equals("AddProductToOrderDetails") &&
+////                    controllerContext.HttpContext.Request.HttpMethod == "POST")
+////                {
+////                    return new Filter[]
+////                    {
+////                    new Filter(_actionFilter, FilterScope.Action, null)
+////                    };
+////                }
+
+////                return new Filter[] { };
+////            }
+////        }
+////    }
+
+
+//    ////PDT
+//    //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.PDTHandler", "Plugins/PaymentPayPalStandard/PDTHandler",
+//    //     new { controller = "PaymentPayPalStandard", action = "PDTHandler" });
+
+//    ////IPN
+//    //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.IPNHandler", "Plugins/PaymentPayPalStandard/IPNHandler",
+//    //     new { controller = "PaymentPayPalStandard", action = "IPNHandler" });
+
+//    ////Cancel
+//    //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.CancelOrder", "Plugins/PaymentPayPalStandard/CancelOrder",
+//    //     new { controller = "PaymentPayPalStandard", action = "CancelOrder" });
+//}
+
+//        /// <summary>
+//        /// Gets a priority of route provider
+//        /// </summary>
+//        public int Priority
+//        {
+//            get { return 100; }
 //        }
 //    }
-
-
-    ////PDT
-    //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.PDTHandler", "Plugins/PaymentPayPalStandard/PDTHandler",
-    //     new { controller = "PaymentPayPalStandard", action = "PDTHandler" });
-
-    ////IPN
-    //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.IPNHandler", "Plugins/PaymentPayPalStandard/IPNHandler",
-    //     new { controller = "PaymentPayPalStandard", action = "IPNHandler" });
-
-    ////Cancel
-    //routeBuilder.MapRoute("Plugin.Payments.PayPalStandard.CancelOrder", "Plugins/PaymentPayPalStandard/CancelOrder",
-    //     new { controller = "PaymentPayPalStandard", action = "CancelOrder" });
-}
-
-        /// <summary>
-        /// Gets a priority of route provider
-        /// </summary>
-        public int Priority
-        {
-            get { return 100; }
-        }
-    }
-}
+//}
