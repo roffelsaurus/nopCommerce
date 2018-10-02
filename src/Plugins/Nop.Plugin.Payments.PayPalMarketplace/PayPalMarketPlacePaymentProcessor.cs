d70ba7b393926b5365fab6b57a6eda45a64073f1
Asbjørn Lucassen <roffelsaurus@gmail.com>
2018-10-02 21:44:44 +0200

    initial commit paypal

---
+++ src/Plugins/Nop.Plugin.Payments.PayPalMarketplace/PayPalMarketPlacePaymentProcessor.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Plugins;
+using Nop.Services.Payments;
+using System;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.Payments.PayPalMarketplace
+{
+    public class PayPalMarketPlacePaymentProcessor : BasePlugin, IPaymentMethod
+    {
+        public bool SupportCapture => throw new NotImplementedException();
+
+        public bool SupportPartiallyRefund => throw new NotImplementedException();
+
+        public bool SupportRefund => throw new NotImplementedException();
+
+        public bool SupportVoid => throw new NotImplementedException();
+
+        public RecurringPaymentType RecurringPaymentType => throw new NotImplementedException();
+
+        public PaymentMethodType PaymentMethodType => throw new NotImplementedException();
+
+        public bool SkipPaymentInfo => throw new NotImplementedException();
+
+        public string PaymentMethodDescription => throw new NotImplementedException();
+
+        public CancelRecurringPaymentResult CancelRecurringPayment(CancelRecurringPaymentRequest cancelPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanRePostProcessPayment(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CapturePaymentResult Capture(CapturePaymentRequest capturePaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetAdditionalHandlingFee(IList<ShoppingCartItem> cart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProcessPaymentRequest GetPaymentInfo(IFormCollection form)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPublicViewComponentName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HidePaymentMethod(IList<ShoppingCartItem> cart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostProcessPayment(PostProcessPaymentRequest postProcessPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProcessPaymentResult ProcessPayment(ProcessPaymentRequest processPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProcessPaymentResult ProcessRecurringPayment(ProcessPaymentRequest processPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RefundPaymentResult Refund(RefundPaymentRequest refundPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> ValidatePaymentForm(IFormCollection form)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VoidPaymentResult Void(VoidPaymentRequest voidPaymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
