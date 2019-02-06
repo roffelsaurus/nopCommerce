4568b6cedee9f23a9cc5baafd2680072ff04323b
Asbjørn Lucassen <roffelsaurus@gmail.com>
2019-02-06 22:24:33 +0100

    price calculation changes

--- src/Libraries/Nop.Services/Catalog/PriceCalculationService.cs
+++ src/Libraries/Nop.Services/Catalog/PriceCalculationService.cs
@@ -782,69 +782,73 @@ namespace Nop.Services.Catalog
         /// <returns>Rounded value</returns>
         public virtual decimal Round(decimal value, RoundingType roundingType)
         {
+            // retrade
+            return Math.Round(value, 5);
+           
+            
             //default round (Rounding001)
-            var rez = Math.Round(value, 5);
-            var fractionPart = (rez - Math.Truncate(rez)) * 10;
+            //var rez = Math.Round(value, 5);
+            //var fractionPart = (rez - Math.Truncate(rez)) * 10;
 
-            //cash rounding not needed
-            if (fractionPart == 0)
-                return rez;
+            ////cash rounding not needed
+            //if (fractionPart == 0)
+            //    return rez;
 
-            //Cash rounding (details: https://en.wikipedia.org/wiki/Cash_rounding)
-            switch (roundingType)
-            {
-                //rounding with 0.05 or 5 intervals
-                case RoundingType.Rounding005Up:
-                case RoundingType.Rounding005Down:
-                    fractionPart = (fractionPart - Math.Truncate(fractionPart)) * 10;
+            ////Cash rounding (details: https://en.wikipedia.org/wiki/Cash_rounding)
+            //switch (roundingType)
+            //{
+            //    //rounding with 0.05 or 5 intervals
+            //    case RoundingType.Rounding005Up:
+            //    case RoundingType.Rounding005Down:
+            //        fractionPart = (fractionPart - Math.Truncate(fractionPart)) * 10;
 
-                    fractionPart = fractionPart % 5;
-                    if (fractionPart == 0)
-                        break;
+            //        fractionPart = fractionPart % 5;
+            //        if (fractionPart == 0)
+            //            break;
 
-                    if (roundingType == RoundingType.Rounding005Up)
-                        fractionPart = 5 - fractionPart;
-                    else
-                        fractionPart = fractionPart * -1;
+            //        if (roundingType == RoundingType.Rounding005Up)
+            //            fractionPart = 5 - fractionPart;
+            //        else
+            //            fractionPart = fractionPart * -1;
 
-                    rez += fractionPart / 100;
-                    break;
-                //rounding with 0.10 intervals
-                case RoundingType.Rounding01Up:
-                case RoundingType.Rounding01Down:
-                    fractionPart = (fractionPart - Math.Truncate(fractionPart)) * 10;
+                //    rez += fractionPart / 100;
+                //    break;
+                ////rounding with 0.10 intervals
+                //case RoundingType.Rounding01Up:
+                //case RoundingType.Rounding01Down:
+                //    fractionPart = (fractionPart - Math.Truncate(fractionPart)) * 10;
 
-                    if (roundingType == RoundingType.Rounding01Down && fractionPart == 5)
-                        fractionPart = -5;
-                    else
-                        fractionPart = fractionPart < 5 ? fractionPart * -1 : 10 - fractionPart;
+                //    if (roundingType == RoundingType.Rounding01Down && fractionPart == 5)
+                //        fractionPart = -5;
+                //    else
+                //        fractionPart = fractionPart < 5 ? fractionPart * -1 : 10 - fractionPart;
 
-                    rez += fractionPart / 100;
-                    break;
-                //rounding with 0.50 intervals
-                case RoundingType.Rounding05:
-                    fractionPart *= 10;
-                    fractionPart = fractionPart < 25 ? fractionPart * -1 : fractionPart < 50 || fractionPart < 75 ? 50 - fractionPart : 100 - fractionPart;
+                //    rez += fractionPart / 100;
+                //    break;
+                ////rounding with 0.50 intervals
+                //case RoundingType.Rounding05:
+                //    fractionPart *= 10;
+                //    fractionPart = fractionPart < 25 ? fractionPart * -1 : fractionPart < 50 || fractionPart < 75 ? 50 - fractionPart : 100 - fractionPart;
 
-                    rez += fractionPart / 100;
-                    break;
-                //rounding with 1.00 intervals
-                case RoundingType.Rounding1:
-                case RoundingType.Rounding1Up:
-                    fractionPart *= 10;
+            //        rez += fractionPart / 100;
+            //        break;
+            //    //rounding with 1.00 intervals
+            //    case RoundingType.Rounding1:
+            //    case RoundingType.Rounding1Up:
+            //        fractionPart *= 10;
 
-                    if (roundingType == RoundingType.Rounding1Up && fractionPart > 0)
-                        rez = Math.Truncate(rez) + 1;
-                    else
-                        rez = fractionPart < 50 ? Math.Truncate(rez) : Math.Truncate(rez) + 1;
+            //        if (roundingType == RoundingType.Rounding1Up && fractionPart > 0)
+            //            rez = Math.Truncate(rez) + 1;
+            //        else
+            //            rez = fractionPart < 50 ? Math.Truncate(rez) : Math.Truncate(rez) + 1;
 
-                    break;
-                case RoundingType.Rounding001:
-                default:
-                    break;
-            }
+            //        break;
+            //    case RoundingType.Rounding001:
+            //    default:
+            //        break;
+            //}
 
-            return rez;
+            //return rez;
         }
 
         #endregion
